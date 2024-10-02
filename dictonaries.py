ddade6dd3f9ffcaf94fdc4343e6995e23a7d6c58
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-02 15:43:28 +0000

    Adding dictionaries

---
+++ dictonaries.py
@@ -0,0 +1,9 @@
+student ={'name':'John','age':25,'courses':['Math','CompSci']}
+
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
+
+for i in student.items():
+    print(i)
