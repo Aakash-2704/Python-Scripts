b196a216a8aca1c54e613317fcfa2c1cd3a0c49e
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-03 23:00:19 +0000

    Adding functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+def f():
+    s = "I have started coding "
+    print(s)
+f()   
+
+print("----------------------------------------------------------------------")
+
+def f():
+    print("Printing inside a function",s)  #Priting inside a function.
+
+#Global scope
+s = "I have started coding "
+f()
+print("Printing outside a function",s)    #Printing outside the function.
+
+print("----------------------------------------------------------------------")
+
+def f():
+    s= "Printing a local variable"      # Local variable
+    print(s)  
+
+s = "Printing a global variable" # Global Variable 
+f()
+print(s)
+
+print("----------------------------------------------------------------------")
