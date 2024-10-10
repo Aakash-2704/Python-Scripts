536d52573580d340d570932f345b0ee2a39926d2
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-10 17:25:40 +0000

    commit

---
+++ substring in string.py
@@ -0,0 +1,13 @@
+# string = abcdefghi
+# substring = def
+# find substring in string 
+
+string = input("")
+substring = input("")
+
+if substring in string:
+    print(f"'{substring}' found in '{string}'")
+else:
+    print(f"'{substring}' not found in '{string}'")
+  
+
