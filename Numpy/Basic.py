e86c62bec7b1c48649aa90854bac488fcdc3b53c
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-14 02:57:26 +0000

    Creating Basic Numpy

---
+++ Numpy/Basic.py
@@ -0,0 +1,15 @@
+import numpy as np
+                   
+print(np.__version__)                     # Version
+
+#==============================================================================
+
+arr = np.array([1, 2, 3, 4, 5])           # Print an array having in a list
+
+print(arr) 
+print(type(arr))
+
+#================================================================================
+arr1 = np.array((1,2,3,4,5))              #Creating an array using tuple
+
+print(arr1)
