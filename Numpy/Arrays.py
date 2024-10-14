6bea62c643e734b0fb51b61c1588dc8865cc2caa
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-14 03:09:36 +0000

    Adding Arrays

---
+++ Numpy/Arrays.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])           # Print an array having in a list
+
+print(arr) 
+print(type(arr))
+
+#================================================================================
+arr_tuple = np.array((1,2,3,4,5))              #Creating an array using tuple
+
+print(arr_tuple)
+
+#=================================================================================
+
+array2d = np.array([[1,2,3],[4,5,6]])       # Creating a 2 dimensional array 
+print(array2d)
+
+#=================================================================================
+
+array3d = np.array([[1,2,3],[4,5,6],[7,8,9]]) #Creating a 3 dimensional array
+print(array3d)
+
+#==================================================================================
+
