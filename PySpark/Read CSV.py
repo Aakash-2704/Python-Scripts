3edc79768e3a8de08245de2b8a3f63164e4516c5
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-14 18:06:26 +0000

    Read CSV File

--- PySpark/Read CSV.py
+++ PySpark/Read CSV.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Read CSV").getOrCreate()
 
-df = spark.read.csv("python-scripts/PySpark/Data/mtcars.csv", header=True, inferSchema=True)
+df = spark.read.csv("/workspaces/python-scripts/PySpark/Data/mtcars.csv", header=True, inferSchema=True)
 
 df.show()
+
