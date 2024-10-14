f89d71ab4b3400d62c34ffdf639678818b6725b0
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-14 18:03:21 +0000

    added csv data

---
+++ PySpark/Read CSV.py
@@ -0,0 +1,7 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Read CSV").getOrCreate()
+
+df = spark.read.csv("python-scripts/PySpark/Data/mtcars.csv", header=True, inferSchema=True)
+
+df.show()
