c817c6b76aa307295cf61d0b9170c97c5db43c58
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-17 03:15:21 +0000

    commit

--- PySpark/Read CSV.py
+++ PySpark/Read CSV.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 
+
 spark = SparkSession.builder.appName("Read CSV").getOrCreate()
 
 df = spark.read.csv("/workspaces/python-scripts/PySpark/Data/mtcars.csv", header=True, inferSchema=True)
