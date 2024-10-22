dbf97d4d8abc1b018ed4d03ef3237ed7e4b88bf7
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-22 16:01:13 +0000

    Adding the updated elements

---
+++ Lists/Updating elements.py
@@ -0,0 +1,13 @@
+fruits =["apple","banana","pineapple"]
+
+print(fruits)
+
+fruits[2] = "blueberry"                # Single element
+
+print(fruits[2])
+
+print(fruits)
+
+fruits[0:3] = ["orange","kiwi","pineapple"] # Mulitple elements
+
+print(fruits)
