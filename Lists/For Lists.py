0eb4f8336b7191decd74f40ee319c98c7f8ea0bc
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-22 18:39:21 +0000

    Adding For in Lists

---
+++ Lists/For Lists.py
@@ -0,0 +1,8 @@
+fruits =["apple","banana","pineapple"]
+
+for fruit in fruits:                  # Printing the list using for loop
+    print(fruit)
+
+
+for index,x in enumerate(fruits):      # Printing the list and it's index
+    print(f"Index {index}:{x}")     
