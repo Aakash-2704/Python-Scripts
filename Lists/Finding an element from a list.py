72499a289c8832b1641a3ecb2789e80df9dda3ff
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-22 17:30:57 +0000

    Finding and Counting the elements from the list

---
+++ Lists/Finding an element from a list.py
@@ -0,0 +1,9 @@
+numbers = [4,0,7,1,2,3,3]
+
+index_of_0 = numbers.index(0)   # Here we are fing the index of an value eg: '0' value is in 1 index
+ 
+print(index_of_0)
+
+count_of_3 = numbers.count(3)   # Count check how many elements are there in the list
+
+print(count_of_3)
