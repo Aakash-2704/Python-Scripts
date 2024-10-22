efe1b0658448d19ad5354475633a42d2ee8f2d89
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-22 16:25:43 +0000

    Adding List Manipulation

---
+++ Lists/List Manipulation.py
@@ -0,0 +1,25 @@
+numbers = [1,2,3]
+
+print(numbers)
+
+numbers.append(4)
+
+print(numbers)                              # Adding a number in the list
+
+numbers.extend([5,6])                      #  Adding multiple elements into the list
+
+print(numbers)
+
+numbers.insert(7,8)                        # In Insert(Index, element) 
+
+print(numbers)
+
+# Removing the elements from the list
+
+numbers.remove(8)                           # Remove the element from the list 
+
+print(numbers)
+
+numbers.pop(0)                             # Pop removes the index value from the list
+
+print(numbers)
