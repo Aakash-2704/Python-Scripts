5db576576369695b1a2d50b19cbcaf3fa63b4297
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-22 17:06:40 +0000

    Adding Sorting and Revering

---
+++ Lists/Lists Sorting and reversing.py
@@ -0,0 +1,19 @@
+numbers = [4,0,7,1,2,3]
+
+print(numbers)        #1st output              
+
+numbers.sort()                         # Soriting the numbers according to ascending order
+
+print(numbers)        # 2nd output
+
+numbers.sort(reverse=True)             # Sorting and reversing the numbers 
+
+print(numbers)        # 3rd output
+
+numbers.reverse()                      # Reversing the given number
+
+print(numbers)         # 4th output
+
+numbers.sort(reversed)
+
+print(numbers)
